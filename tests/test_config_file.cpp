e9a76af63fb4a74b6fc14cda8c70dd55568168c3
Sylwester Fraczek <uberloss@gmail.com>
2020-10-28 17:33:50 +0100

    update unit tests

--- tests/test_config_file.cpp
+++ tests/test_config_file.cpp
@@ -46,34 +46,91 @@ TEST_CASE("Read Config from file", "config_file")
     TemporaryFile tmp_config_file;
     tmp_config_file.Write(default_config);
     Config config(tmp_config_file.Name());
-    auto& drums = config.drums;
 
-    REQUIRE(drums.size() == 3);
+    REQUIRE(config.title.text == "Ion Audio IED08 Drum Rocker (R) for PlayStation (R) binding");
+    REQUIRE(config.title.rgb == std::vector<uint8_t>{0, 111, 205});
+
+    auto &drums = config.drums;
+
+    REQUIRE(drums.size() == 8);
 
     REQUIRE(drums[0].name == "HiHat");
     REQUIRE(drums[0].sound_file == "sounds/Analog Hihat 1.ogg");
     REQUIRE(drums[0].image_file == "images/hihat/drum-kit-hi-hat.jpg");
     REQUIRE(drums[0].keyboard_buttons_combination == std::vector<uint32_t>{7});
-    REQUIRE(drums[0].joystick_buttons_combination == std::vector<uint32_t>{0});
-    REQUIRE(drums[0].position_on_screen.x == 100);
-    REQUIRE(drums[0].position_on_screen.y == 100);
+    REQUIRE(drums[0].joystick_buttons_combination == std::vector<uint32_t>{3, 11, 107});
+    REQUIRE(drums[0].position_on_screen.x == 200);
+    REQUIRE(drums[0].position_on_screen.y == 150);
     REQUIRE(drums[0].radius == 80);
+    REQUIRE(drums[0].rgb == std::vector<uint8_t>{255, 255, 0});
 
     REQUIRE(drums[1].name == "Snare");
     REQUIRE(drums[1].sound_file == "sounds/Analog Snare 1.ogg");
     REQUIRE(drums[1].image_file == "images/snare/rogers 1964 silver sparkle powertone (cleveland)_thumb.jpg");
     REQUIRE(drums[1].keyboard_buttons_combination == std::vector<uint32_t>{18});
-    REQUIRE(drums[1].joystick_buttons_combination == std::vector<uint32_t>{1});
-    REQUIRE(drums[1].position_on_screen.x == 300);
-    REQUIRE(drums[1].position_on_screen.y == 100);
+    REQUIRE(drums[1].joystick_buttons_combination == std::vector<uint32_t>{2, 10});
+    REQUIRE(drums[1].position_on_screen.x == 100);
+    REQUIRE(drums[1].position_on_screen.y == 350);
     REQUIRE(drums[1].radius == 80);
+    REQUIRE(drums[1].rgb == std::vector<uint8_t>{255, 0, 0});
 
-    REQUIRE(drums[2].name == "Kick");
-    REQUIRE(drums[2].sound_file == "sounds/Analog Kick 1.ogg");
-    REQUIRE(drums[2].image_file == "images/kick/drum-kit-pedal.jpg");
-    REQUIRE(drums[2].keyboard_buttons_combination == std::vector<uint32_t>{7, 18});
-    REQUIRE(drums[2].joystick_buttons_combination == std::vector<uint32_t>{2});
-    REQUIRE(drums[2].position_on_screen.x == 600);
-    REQUIRE(drums[2].position_on_screen.y == 100);
+    REQUIRE(drums[2].name == "Tom-tom1");
+    REQUIRE(drums[2].sound_file == "sounds/General Room Hi Tom.ogg");
+    REQUIRE(drums[2].image_file == "images/tom/drum-kit-1488406125CbG.jpg");
+    REQUIRE(drums[2].keyboard_buttons_combination == std::vector<uint32_t>{999});
+    REQUIRE(drums[2].joystick_buttons_combination == std::vector<uint32_t>{3, 10});
+    REQUIRE(drums[2].position_on_screen.x == 300);
+    REQUIRE(drums[2].position_on_screen.y == 300);
     REQUIRE(drums[2].radius == 80);
+    REQUIRE(drums[2].rgb == std::vector<uint8_t>{255, 255, 0});
+
+    REQUIRE(drums[3].name == "Tom-tom2");
+    REQUIRE(drums[3].sound_file == "sounds/General Room Mid Tom.ogg");
+    REQUIRE(drums[3].image_file == "images/tom/drum-kit-1488406125CbG.jpg");
+    REQUIRE(drums[3].keyboard_buttons_combination == std::vector<uint32_t>{999});
+    REQUIRE(drums[3].joystick_buttons_combination == std::vector<uint32_t>{0, 10});
+    REQUIRE(drums[3].position_on_screen.x == 500);
+    REQUIRE(drums[3].position_on_screen.y == 300);
+    REQUIRE(drums[3].radius == 80);
+    REQUIRE(drums[3].rgb == std::vector<uint8_t>{0, 0, 255});
+
+    REQUIRE(drums[4].name == "Tom-tom3");
+    REQUIRE(drums[4].sound_file == "sounds/General Room Low Tom.ogg");
+    REQUIRE(drums[4].image_file == "images/tom/drum-kit-1488406125CbG.jpg");
+    REQUIRE(drums[4].keyboard_buttons_combination == std::vector<uint32_t>{999});
+    REQUIRE(drums[4].joystick_buttons_combination == std::vector<uint32_t>{1, 10});
+    REQUIRE(drums[4].position_on_screen.x == 700);
+    REQUIRE(drums[4].position_on_screen.y == 350);
+    REQUIRE(drums[4].radius == 80);
+    REQUIRE(drums[4].rgb == std::vector<uint8_t>{0, 255, 0});
+
+    REQUIRE(drums[5].name == "Crash");
+    REQUIRE(drums[5].sound_file == "sounds/General Crash.ogg");
+    REQUIRE(drums[5].image_file == "images/cymbal/320px-Bellotti_Cymbal.jfif");
+    REQUIRE(drums[5].keyboard_buttons_combination == std::vector<uint32_t>{999});
+    REQUIRE(drums[5].joystick_buttons_combination == std::vector<uint32_t>{1, 11, 107});
+    REQUIRE(drums[5].position_on_screen.x == 600);
+    REQUIRE(drums[5].position_on_screen.y == 150);
+    REQUIRE(drums[5].radius == 80);
+    REQUIRE(drums[5].rgb == std::vector<uint8_t>{0, 255, 0});
+
+    REQUIRE(drums[6].name == "Ride");
+    REQUIRE(drums[6].sound_file == "sounds/General Ride.ogg");
+    REQUIRE(drums[6].image_file == "images/cymbal/320px-Bellotti_Cymbal.jfif");
+    REQUIRE(drums[6].keyboard_buttons_combination == std::vector<uint32_t>{999});
+    REQUIRE(drums[6].joystick_buttons_combination == std::vector<uint32_t>{0, 11, 107});
+    REQUIRE(drums[6].position_on_screen.x == 400);
+    REQUIRE(drums[6].position_on_screen.y == 100);
+    REQUIRE(drums[6].radius == 80);
+    REQUIRE(drums[6].rgb == std::vector<uint8_t>{0, 0, 255});
+
+    REQUIRE(drums[7].name == "Kick");
+    REQUIRE(drums[7].sound_file == "sounds/Analog Kick 1.ogg");
+    REQUIRE(drums[7].image_file == "images/kick/drum-kit-pedal.jpg");
+    REQUIRE(drums[7].keyboard_buttons_combination == std::vector<uint32_t>{7, 18});
+    REQUIRE(drums[7].joystick_buttons_combination == std::vector<uint32_t>{4});
+    REQUIRE(drums[7].position_on_screen.x == 400);
+    REQUIRE(drums[7].position_on_screen.y == 500);
+    REQUIRE(drums[7].radius == 80);
+    REQUIRE(drums[7].rgb == std::vector<uint8_t>{255, 128, 0});
 }
--- tests/test_lib.cpp
+++ tests/test_lib.cpp
@@ -8,15 +8,15 @@ TEST_CASE("Public variables", "lib")
 {
     App app;
 
-    REQUIRE(app.window_size.width == 800);
-    REQUIRE(app.window_size.height == 600);
+    REQUIRE(app.window_size.width == 1024);
+    REQUIRE(app.window_size.height == 768);
     REQUIRE(app.window_title == "Ion Drum Pad");
     REQUIRE(app.config_path == "config.json");
-    #ifdef _WIN32
+#ifdef _WIN32
     REQUIRE(app.default_font == "C:\\Windows\\Fonts\\arial.ttf");
-    #elif __linux__
+#elif __linux__
     REQUIRE(app.default_font == "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
-    #endif
+#endif
     REQUIRE(app.ms_delay.count() == 16);
 }
 
@@ -27,10 +27,15 @@ TEST_CASE("Callbacks don't throw", "lib")
     auto key_code = 12u;
     auto joystick_id = 1u;
     auto joystick_button = 5u;
+    auto axis = 7u;
+    auto force = 100u;
     REQUIRE_NOTHROW(app.KeyPressedCallback(12));
     REQUIRE_NOTHROW(app.KeyReleasedCallback(12));
     REQUIRE_NOTHROW(app.JoystickButtonPressedCallback(joystick_id, joystick_button));
     REQUIRE_NOTHROW(app.JoystickButtonReleasedCallback(joystick_id, joystick_button));
     REQUIRE_NOTHROW(app.JoystickDisconnectedCallback(joystick_id));
     REQUIRE_NOTHROW(app.JoystickConnectedCallback(joystick_id));
+    REQUIRE_NOTHROW(app.JoystickMovedCallback(axis, force));
+    // Cannot have twice the same button presset at the same time
+    REQUIRE(app.joystick_buttons_pressed == std::vector<uint32_t>{joystick_button, axis + app.button_axis_offset});
 }
